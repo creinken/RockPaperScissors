436a96c7abe299ed02d68948d25af784b279759a
creinken <reinkendigital@gmail.com>
2025-05-14 05:22:10 -0500

    Add play round function
    Added function called playRound that takes a player and compputer
    choices, checks for a winner, adds to the appropriate score and
    tells the user if they won or lost or tied.

--- script.js
+++ script.js
@@ -28,3 +28,78 @@ function getHumanChoice() {
     let userChoice = prompt("Please enter a choice between Rock, Paper and Scissors");
     return userChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    let result = ""
+    switch (humanChoice.toLowerCase()) {
+        case "rock":
+            switch (computerChoice) {
+                case "Rock":
+                    result = "tie";
+                    break;
+                case "Paper":
+                    result = "lose";
+                    break;
+        
+                case "Scissors":
+                    result = "win";
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        
+        case "paper":
+            switch (computerChoice) {
+                case "Rock":
+                    result = "win";
+                    break;
+                case "Paper":
+                    result = "tie";
+                    break;
+        
+                case "Scissors":
+                    result = "lose";
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        
+        case "scissors":
+            switch (computerChoice) {
+                case "Rock":
+                    result = "lose";
+                    break;
+                case "Paper":
+                    result = "win";
+                    break;
+        
+                case "Scissors":
+                    result = "tie";
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+        
+        default:
+            break;
+    } // end of humanChoice switch
+
+    if (result = "win") {
+        humanScore += 1;
+    } else if (result = "lose") {
+        computerScore += 1;
+    }
+
+    console.log("You " + result + ".");
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
